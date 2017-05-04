9f2bc412146545b470627da40197d06f61b69f81
FitzJay <elias.p.benson@gmail.com>
2017-05-03 21:57:14 -0700

    updated html

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+script(document).ready(function() {
+
+	var characters = {
+
+		"hanSolo": {
+			name: "Hans_Solo"
+			health: 
+			attack:
+			imageUrl:
+			enemyAttackBack: 
+		},
+		"r2d2": {
+			name: "R2D2"
+			health:
+			attack:
+			imageURL:
+			enemyAttackBack:
+		},
+		"vader": {
+			name: "Darth_Vader"
+			health:
+			attack:
+			imageURL:
+			enemyAttackBack:
+		},
+		"yoda": {
+			name: "Yoda"
+			health:
+			attack:
+			imageURL:
+			enemyAttackBack:
+		},
+
+	};
+
+	var currentselectedCharacter;
+	var currentDefender;
+	var fighters =[];
+	var indexofselChar;
+	var attackResult;
+	var turnCounter = 1;
+	var killCount = 0;
+
+	
+
+
+
+
+
+}
